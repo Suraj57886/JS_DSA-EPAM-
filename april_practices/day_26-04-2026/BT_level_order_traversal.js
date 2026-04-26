7025435494d0ca0076589d4bc8ccac045983d446
Suraj Kandagatla <suraj_kandagatla@epam.com>
2026-04-26 13:02:03 +0530

    added 7 on 26-04-2026

---
+++ april_practices/day_26-04-2026/BT_level_order_traversal.js
@@ -0,0 +1,39 @@
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -1000 <= Node.val <= 1000
+var levelOrder = function(root) {
+    let levels=[]
+    function dfs(root,index){
+        if(root===null){
+            return;
+        }
+        if(levels.length===index){
+            levels.push([])
+        }
+        levels[index].push(root.val);
+        dfs(root.left,index+1)
+        dfs(root.right,index+1)
+    }
+    dfs(root,0)
+    return levels;
+};
---
+++ april_practices/day_26-04-2026/symmetric_tree.js
@@ -0,0 +1,33 @@
+// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true
+// Example 2:
+
+
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 1000].
+// -100 <= Node.val <= 100
+var isSymmetric = function(root) {
+    if(!root) return true;
+    function isMirror(Leftroot,Rightroot){
+        if(!Leftroot && !Rightroot){
+            return true;
+        }
+        if(!Leftroot || !Rightroot){
+            return false;
+        }
+        return Leftroot.val===Rightroot.val && isMirror(Leftroot.left,Rightroot.right) && isMirror(Leftroot.right,Rightroot.left)
+    }
+    return isMirror(root.left,root.right)
+};
